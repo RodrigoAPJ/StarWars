22e7d68e521adc173526641e016296c67062efbf
Daniel Magaña <daniel.magana@sansano.usm.cl>
2021-12-05 17:21:56 -0300

    Retornos fulcrum
    Comentarios de qué deberia hacer cada funcion en fulcrum y retornos arreglados segun los proto

--- Fulcrum/fulcrum.go
+++ Fulcrum/fulcrum.go
@@ -15,17 +15,33 @@ type server struct {
 	grpc_fulcrum.UnimplementedFulcrumServer
 }
 
-func (s *server) F_GetNumberRebels(ctx context.Context, in *F_FromLeia) (*F_ToLeia, error) {
-	return nil, nil
+func (s *server) F_GetNumberRebels(ctx context.Context, in *grpc_fulcrum.F_FromLeia) (*grpc_fulcrum.F_ToLeia, error) {
+	//Retornar numero de rebeldes en ciudad x
+	//Quizas desencadene un merge
+	return grpc_fulcrum.F_ToLeia{FRebeldes   : grpc_broker.F_Rebeldes{Cantidad: },
+								FReloj    : grpc_broker.F_Reloj{X: , Y: , Z: }, 
+							 	}, nil
 }
-func (s *server) F_SendCommand(ctx context.Context, in *F_From_Informante) (*F_To_Informante, error) {
-	return nil, nil
+
+func (s *server) F_SendCommand(ctx context.Context, in *grpc_fulcrum.F_From_Informante) (*grpc_fulcrum.F_To_Informante, error) {
+	//Función que llamarán los informantes para mandar comandos
+	return grpc_fulcrum.F_To_Informante{FReloj    : grpc_broker.F_Reloj{X: , Y: , Z: }, 
+										FLog      : grpc_broker.F_Log{Log: }
+										}, nil
 }
-func (s *server) F_Request(ctx context.Context, in *Fantasma) (*F_Merge_Data, error) {
-	return nil, nil
+
+func (s *server) F_Request(ctx context.Context, in *grpc_fulcrum.Fantasma) (*grpc_fulcrum.F_Merge_Data, error) {
+	//Funcion que llamará un servidor fulcrum a los otros dos servidores para comenzar el proceso de merge
+	//Se deberán retornar los logs y relojes pertinentes para que el nodo que hizo la request
+	//determine como hacer el merge
+	return grpc_fulcrum.F_Merge_Data{FReloj    : grpc_broker.F_Reloj{X: , Y: , Z: }, 
+									FLog      : grpc_broker.F_Log{Log: }
+									}, nil
 }
-func (s *server) F_Merge(ctx context.Context, in *F_Merge_Data) (*Fantasma, error) {
-	return nil, nil
+
+func (s *server) F_Merge(ctx context.Context, in *grpc_fulcrum.F_Merge_Data) (*grpc_fulcrum.Fantasma, error) {
+	//Se envian los datos de como debe quedar todo con el merge desde el nodo que lo realizó
+	return grpc_fulcrum.Fantasma{Fantasma: 1}, nil
 }
 
 //Direcciones de los servidores fulcrum
